61e5397e34a5960c8d6630ed3c90531432b93445
JnBanks <joku.banks@gmail.com>
2022-05-10 10:41:50 -0700

    created seeds

---
+++ seeds/index.js
@@ -0,0 +1,28 @@
+const sequelize = require("../config/connection")
+const {User,Event} = require("../models")
+
+const userData = require('./userData.json');
+const eventData = require('./eventData.json');
+
+const seedDatabase = async () => {
+    await sequelize.sync({ force: true });
+
+    const events = await Event.bulkCreate(eventData, {
+        individualHooks: true,
+        returning: true,
+    });
+
+    const users = await User.bulkCreate(userData, {
+        individualHooks: true,
+        returning: true,
+    });
+
+    // const cats = await Cat.bulkCreate(catData, {
+    //     individualHooks: true,
+    //     returning: true,
+    // })
+
+    process.exit(0);
+};
+
+seedDatabase();
